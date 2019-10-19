fc7a05b217e6296fc266d6acdd73a16584e45925
lazynessmind <lazynessmind@gmail.com>
2019-10-19 01:52:09 +0100

    added the version check

--- src/main/java/com/lazynessmind/horsemodifiers/registry/RegistryEvents.java
+++ src/main/java/com/lazynessmind/horsemodifiers/registry/RegistryEvents.java
@@ -1,16 +1,19 @@
 package com.lazynessmind.horsemodifiers.registry;
 
+import com.lazynessmind.horsemodifiers.configs.Configs;
 import com.lazynessmind.horsemodifiers.interfaces.IHasModel;
-import com.lazynessmind.horsemodifiers.items.HMItem;
 import com.lazynessmind.horsemodifiers.items.HMItems;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -32,16 +35,18 @@ public class RegistryEvents {
         }
     }
 
-    //Maybe still broken at the moment
     @SubscribeEvent
     public static void onWorldLoaded(EntityJoinWorldEvent joinWorldEvent) {
-        System.out.println("Update");
-        if (!joinWorldEvent.getWorld().isRemote) {
-            if (joinWorldEvent.getEntity() instanceof EntityPlayer) {
-                EntityPlayer player = (EntityPlayer) joinWorldEvent.getEntity();
-                if (ForgeVersion.getStatus() == ForgeVersion.Status.OUTDATED) {
-                    System.out.println("Update");
-                    player.sendMessage(new TextComponentString(TextFormatting.BOLD + "[Farming Tools]" + TextFormatting.RED + " Current version is outdated! " + TextFormatting.WHITE + "Check the mod page to update. :)"));
+        World world = joinWorldEvent.getWorld();
+        Entity entity = joinWorldEvent.getEntity();
+
+        if (Configs.showUpdateMessage) {
+            if (!world.isRemote) {
+                if (entity instanceof EntityPlayer) {
+                    EntityPlayer player = (EntityPlayer) entity;
+                    if (ForgeVersion.getResult(Loader.instance().activeModContainer()).status == ForgeVersion.Status.OUTDATED) {
+                        player.sendMessage(new TextComponentString(TextFormatting.BOLD + "[Farming Tools]" + TextFormatting.RED + " Current version is outdated! " + TextFormatting.WHITE + "Check the mod page to update. :)"));
+                    }
                 }
             }
         }
